7717cc59eca0a96f2bb8d88641f49c7302326998
oscarjimenez4777 <oscarjimenez4777@outlook.com>
2021-09-30 15:42:33 -0400

    Load customers

---
+++ mobile/flutter_contacts/lib/services/customer_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/models/customer.dart';
+import 'package:http/http.dart' as http;
+
+class CustomerService extends ChangeNotifier {
+  final String _baseUrl = '192.168.1.115:8000';
+
+  final List<Customer> customers = [];
+  bool isLoading = true;
+
+  CustomerService() {
+    this.loadCustomers();
+  }
+
+  //Fetch Data
+
+  Future loadCustomers() async {
+    final url = Uri.http(_baseUrl, 'api/customers');
+    print(url);
+
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> customerMap = json.decode(resp.body);
+
+    customerMap.forEach((key, value) {
+      final tempCustomer = Customer.fromMap(value);
+      this.customers.add(tempCustomer);
+    });
+  }
+}
