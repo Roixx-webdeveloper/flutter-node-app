8cebdc1c4676ba5ddde9f979c25333f394a4b002
oscarjimenez4777 <oscarjimenez4777@outlook.com>
2021-09-30 13:28:50 -0400

    Customer model

---
+++ mobile/flutter_contacts/lib/models/customer.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class Customer {
+  Customer({
+    this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.rating,
+  });
+
+  int? id;
+  String name;
+  String phone;
+  String email;
+  String rating;
+
+  factory Customer.fromJson(String str) => Customer.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Customer.fromMap(Map<String, dynamic> json) => Customer(
+      id: json["id"],
+      name: json["name"],
+      phone: json["phone"],
+      email: json["email"],
+      rating: json["rating"]);
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "rating": rating
+      };
+
+  Customer copy() => Customer(
+      name: this.name,
+      phone: this.phone,
+      email: this.email,
+      rating: this.rating);
+}
